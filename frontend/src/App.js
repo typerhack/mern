786fe286020d08fa6b51efe7f690486f185b65d5
Daniel Dolatabadi Surface Pro <typerhack@gmail.com>
2022-04-10 11:28:52 +0430

    React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi from react</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
