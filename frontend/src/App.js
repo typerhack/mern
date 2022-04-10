ef22c960a90d2c5777a3a9bf2ec88e43e74d128a
Daniel Dolatabadi Surface Pro <typerhack@gmail.com>
2022-04-10 16:47:26 +0430

    Backend Initialization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,27 @@
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hi from react</h1>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </>
+    </Router>
   );
-}
+};
 
 export default App;
